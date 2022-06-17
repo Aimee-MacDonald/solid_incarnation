3b707de34541bfc0027c05f62b7d6b95bbe5a4e0
Aimee MacDonald <aimeelmacdonald@gmail.com>
2022-06-17 17:37:56 +0200

    Server setup

---
+++ src/backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const path = require('path')
+
+//  app.use(express.static(path.join(__dirname, '../../dist')))
+
+app.get('*', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.listen(process.env.PORT, console.log('Server up'))
