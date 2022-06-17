49571a65e91342345e45c013f6e7b6c5f7c4b350
Aimee MacDonald <aimeelmacdonald@gmail.com>
2022-06-17 21:57:34 +0200

    React setup

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,7 +3,7 @@ const app = express()
 require('dotenv').config()
 const path = require('path')
 
-//  app.use(express.static(path.join(__dirname, '../../dist')))
+app.use(express.static(path.join(__dirname, '../../dist')))
 
 app.get('*', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '/index.html'))
---
+++ src/frontend/main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+import 'normalize.css'
+import './main.sass'
+
+import App from './App/App'
+
+const root = createRoot(document.getElementById('root'))
+root.render(<App/>)
